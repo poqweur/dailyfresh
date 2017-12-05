5f56624cd1674bbbb0a93e501cba7bdda1c9629b
练习用户1 <1149447019@qq.com>
2017-12-05 16:04:54 +0800

    创建模型父类

--- db/base_model.py
+++ db/base_model.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class BaseModels(models.Model):
+    create_time=models.DateTimeField(verbose_name='创建时间',auto_now_add=True)#auto_now_add就是设置创建时间的参数
+    update_time=models.DateTimeField(verbose_name='更新时间',auto_now=True)#auto_now设置更新时间
+    is_delete=models.BooleanField(default=False,verbose_name='删除标记')
+
+    class Meta:
+        #说明这是个抽象类
+        abstract=True
